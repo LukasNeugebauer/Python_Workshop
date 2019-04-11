310e29ccdd81b87755684548735c4fd14224f9fa
LukasNeugebauer <l.neugebauer@uke.de>
2019-04-11 19:04:38 +0200

    workflow part 2

--- example_workflow/Base_project.py
+++ example_workflow/Base_project.py
@@ -11,7 +11,8 @@ class Base_project:
     
     project = 'Example_project';
     path = 'C:/Users/neugebauer/Documents/Python_workshop/toy_data/';
-    
+    file_map = {'food': 'food_ratings.csv', 'rate': 'rating_sequence', 'RT': 'sequence_RT.csv'};
+
     #you need one anyway
     def __init__(self):
         self.list_subjects();
@@ -25,9 +26,12 @@ class Base_project:
     def subject_exists(self,subID):
         return 's{:02d}'.format(subID) in self.subjects  
     
-    def data_exists( self, subID, filetype):
-        file_map = {'food': 'food_ratings.csv', 'rate': 'rating_sequence', 'RT': 'sequence_RT.csv'};
-        if not filetype in file_map.keys():
-            raise KeyError( "filetype has to be one of the following: ['food','rate','RT']")
-        path = os.path.join( self.path, 's{:02d}'.format(subID), file_map[filetype]);
+    def data_exists(*args, **kwargs): # you could also just ask for subID and filetype. if there are many input arguments, this is easier.
+        path = self.path_to_data(*args, **kwargs);
         return os.path.exists(path)
+
+    def path_to_data(self, subID, filetype):
+        if not filetype in self.file_map.keys():
+            raise KeyError( "filetype has to be one of the following: ['food','rate','RT']")
+        path = os.path.join( self.path, 's{:02d}'.format(subID), self.file_map[filetype]);
+        return path
